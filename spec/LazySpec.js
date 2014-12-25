a33c7a93b1e07c11ee4e4fb7582200e2fb9227bd
Carlos De Dios <c.dedios@outlook.com>
2014-12-25 03:59:15 -0400

    Adding as many tests as I could think of (I could probably do more but is 4am in the morning)

---
+++ spec/LazySpec.js
@@ -0,0 +1,93 @@
+try {
+	var fs = require('fs');
+	eval(fs.readFileSync('../src/Lazy.js') + '');
+}
+catch (e) {
+	console.error(e); 
+}
+
+try {
+	function exactEquality(arr1, arr2) {
+		var equals = true;
+
+			var list = arr1.sort();
+			var resultList = arr2.sort();
+
+			for (var i = 0; i < (list.length + resultList.length) / 2; i++) {
+				equals = equals && ((list[i] || -1 ) === (resultList[i] || -1));
+			}
+
+			return equals;
+	};
+
+	describe("Lazy.js functionality", function() {
+		var lazy;
+		var DefaultData = [1, 2, 3, 4, 5, 6, 7];
+		
+		beforeEach(function() {
+			lazy = new Lazy(DefaultData);
+		});
+
+		it("should return the same collection if invoked without any specification", function(){
+			var list = DefaultData;
+			var resultList = lazy.invoke();
+			var allExist = exactEquality(list, resultList);
+
+			expect(allExist).toEqual(true);
+		});
+
+		it("should get the data transformed the way it's specified", function() {
+			var list = DefaultData.map(function(x) { return x * 2; });
+			var resultList = lazy.get(function (x) { return x * 2; }).invoke();
+			var allExist = exactEquality(list, resultList);
+
+			expect(allExist).toEqual(true);
+		});
+
+		it("should filter as specified", function(){
+			var list = DefaultData.filter(function (x) { return x == 5 || x == 7 });
+			var resultList = lazy.where(function (x) { return x == 5 || x == 7 }).invoke();
+			var allExist = exactEquality(list, resultList);
+
+			expect(allExist).toEqual(true);
+		});
+
+		it("should return first item if .first has no specified matching criteria", function() {
+			var result = lazy.first().invoke();
+			expect(result).toEqual(1);
+		});
+
+		it("should return first item matching the specified criteria", function () {
+			var result = lazy.first(function (x) { return x > 3 && x < 5; }).invoke();
+			expect(result).toEqual(4);
+		});
+
+		it("should have no difference between stack and several add", function() {
+			lazy = new Lazy("The lazy dog");
+			var result =
+			lazy
+				.add(function (x) { return x.toLowerCase(); })
+				.add(function (x) { return x.replace(/[ ]/g, '') })
+				.invoke();
+			var result2 =
+			lazy
+				.stack(
+					[function (x) { return x.toLowerCase(); },
+					 function (x) { return x.replace(/[ ]/g, '') }])
+				.invoke();
+
+			var equals = (result == result2);
+			expect(equals).toEqual(true);
+		});
+
+		it("should invoke itself if requested for comparission without the invoke", function() {
+			var number = 6;
+			var result = lazy.first(function (x) { return x == 6; });
+			var equals = (number == result);
+			expect(equals).toEqual(true);
+		});
+	});
+}
+catch (e) {
+	console.error(e);
+}
